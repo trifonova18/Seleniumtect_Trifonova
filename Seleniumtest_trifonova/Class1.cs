ca237ad561b64c4807c22c337cd14adb12d8433b
Trifonova Nataliya <natalia.o.g@mail.ru>
2024-04-20 17:20:16 +0300

    Добавила 5 тестов

--- Seleniumtest_trifonova/Class1.cs
+++ Seleniumtest_trifonova/Class1.cs
@@ -1,35 +1,114 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
-namespace Seleniumtest_trifonova;
+namespace Seleniun_trifonova;
 
 public class Seleniumtest
 {
-    [Test]
-    public void Authorization()
-    {
-        var options = new ChromeOptions();
-        options.AddArgument("--start-maximized");
 
-        var driver = new ChromeDriver(options);
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        Thread.Sleep(4000);
+public ChromeDriver driver;
 
-        var login = driver.FindElement(By.XPath("//*[@id=\"Username\"]"));
-        login.SendKeys("natalia.o.g@mail.ru");
-        
-        var password = driver.FindElement(By.XPath("//*[@id=\"Password\"]"));
-        password.SendKeys("Rbgfhbc1987!!!@%");
+public string staff = "https://staff-testing.testkontur.ru";
 
-        var enter = driver.FindElement(By.XPath("//*[@id=\"login_form\"]/div[5]/button"));
-        enter.Click();
+[SetUp]
+public void SetUp()
 
-        Thread.Sleep(3000);
-
-        var currentUrl = driver.Url;
-        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");   
-        
-        driver.Quit();
-    }
+{ var options = new ChromeOptions();
+    options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+    driver = new ChromeDriver(options);
+    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 }
+[TearDown]
+public void TearDown()
+{
+    driver.Close();
+    driver.Quit();
+}
+
+public void Auth(string user, string password)
+{
+    driver.Navigate().GoToUrl(staff);
+    driver.FindElement(By.Id("Username")).SendKeys(user);
+    driver.FindElement(By.Name("Password")).SendKeys(password);
+    driver.FindElement(By.Name("button")).Click();
+    Assert.That(driver.FindElement(By.CssSelector("h1[data-tid='Title']")).Displayed, "Не удалось авторизоваться");
+}
+
+[Test]
+public void Authorization()
+{
+    driver.Navigate().GoToUrl(staff);
+    
+    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); 
+    wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("Username")));
+    
+    var login = driver.FindElement(By.Id("Username"));
+    login.SendKeys("natalia.o.g@mail.ru");
+    
+    var password = driver.FindElement(By.Name("Password"));
+    password.SendKeys("Rbgfhbc1987!!!@%");
+    
+    var enter = driver.FindElement(By.Name("button"));
+    enter.Click();
+    
+    Assert.That(driver.FindElement(By.CssSelector("h1[data-tid='Title']")).Displayed, "Не получилось авторизироваться");
+
+}
+
+[Test]
+public void TestSearch()
+{
+    Auth("natalia.o.g@mail.ru", "Rbgfhbc1987!!!@%");
+    driver.Navigate().GoToUrl(staff);
+    driver.FindElement(By.CssSelector("span[data-tid='SearchBar']")).Click();
+    driver.FindElement(By.CssSelector("span[data-tid='SearchBar'] input")).SendKeys("Трифонова");
+    driver.FindElement(By.CssSelector("div[title='Наталья Трифонова']")).Click();
+    var employeeName = driver.FindElement(By.CssSelector("div[data-tid='EmployeeName']")).Text;
+    var expectedEmployeeName = "Наталья Трифонова";
+    Assert.That(employeeName == expectedEmployeeName,"Ожидаемый результат {employeeName} отличается от ожидаемого {expectedEmployeeName}");
+}
+[Test]
+public void Logout()
+{
+    Auth("natalia.o.g@mail.ru", "Rbgfhbc1987!!!@%");
+    driver.Navigate().GoToUrl(staff);
+    driver.FindElement(By.CssSelector("[data-tid='Avatar']")).Click();     
+    driver.FindElement(By.CssSelector("[data-tid='Logout']")).Click(); 
+    var logoutText = driver.FindElement(By.CssSelector("h3")).Text;
+    var expectedLogoutText = "Вы вышли из учетной записи";
+    Assert.That(logoutText == expectedLogoutText, "Фактический текст {logoutText} отличается от ожидаемого {expectedLogoutText}");
+}
+
+[Test]
+public void newCommunity()
+{
+    Auth("natalia.o.g@mail.ru", "Rbgfhbc1987!!!@%");
+    driver.Navigate().GoToUrl(staff + "/communities");
+    driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section/div[2]/span/button")).Click();
+    driver.FindElement(By.CssSelector("textarea[placeholder='Название сообщества']")).SendKeys("Cats");
+    driver.FindElement(By.CssSelector("textarea[placeholder='Описание сообщества']")).SendKeys("Cute Cats");
+    driver.FindElement(By.XPath("//span[contains(text(), 'Создать')]")).Click();
+    driver.FindElement(By.CssSelector("button[data-tid='DeleteButton']")).Click();
+    driver.FindElement(By.XPath("//span[contains(text(), 'Удалить')]")).Click();
+}
+[Test]
+public void addAddress()
+{
+    Auth("natalia.o.g@mail.ru", "Rbgfhbc1987!!!@%");
+    driver.FindElement(By.CssSelector("[data-tid='Avatar']")).Click();
+    driver.FindElement(By.CssSelector("[data-tid='ProfileEdit']")).Click();
+    driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section[3]/div[1]/div[6]/label[2]/div/div/textarea")).SendKeys("Санкт-Петербург");
+    driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section[1]/div[2]/button[1]")).Click();
+    var AdressText = driver
+        .FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section[2]/div[2]/div[2]/div/div[2]")).Text;
+    var expectedAdressText = "Санкт-Петербург";
+    Assert.That(AdressText == expectedAdressText, "Адрес добавлен");
+}
+
+}
+
+
